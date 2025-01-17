f4d0ce446681e2b12a285a1cc14c8e3ab4792da9
unknown <aframawandani@gmail.com>
2025-01-17 07:05:37 +0700

    init bot with wwebjs

--- src/client/bot.ts
+++ src/client/bot.ts
@@ -1,1 +1,6 @@
+import { Client, LocalAuth } from "whatsapp-web.js";
 
+export const bot = new Client({
+  authStrategy: new LocalAuth(),
+  takeoverOnConflict: true,
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,58 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { bot } from "./client/bot";
+import { toDataURL } from "qrcode";
+import qrcode from "qrcode-terminal";
 
-const app = express()
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+// mutable variable
+let QRCodeURL: string | null = null;
+
+// configure express options
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+bot.on("qr", (qr: string) => {
+  toDataURL(qr).then((url: string) => {
+    QRCodeURL = url;
+  });
+  qrcode.generate(qr, { small: true });
+});
+
+bot.on("ready", () => {
+  QRCodeURL = null;
+  console.log("Client is ready!");
+});
+
+// init bot
+bot.initialize();
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("index", {
+    title: "WhatsApp Gateway",
+    message: "Welcome to WhatsApp Gateway!",
+  });
+});
+
+app.get("/qrcode", async (req: Request, res: Response) => {
+  if (QRCodeURL) {
+    res.render("qrcode", {
+      qrimage: QRCodeURL,
+    });
+  } else {
+    setTimeout(() => {
+      res.render("qrcode", {
+        qrimage: QRCodeURL,
+      });
+    }, 5000);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`app is running on port : ${port}`)
-})
+  console.log(`app is running on port : ${port}`);
+});
